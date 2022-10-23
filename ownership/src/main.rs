0ce014932d62a08506b63194ed85b2d4049a49dc
Makcime <marlier.maxime@gmail.com>
2022-10-23 22:05:16 +0200

    Day 4, part 2

---
+++ ownership/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+        let mut s = String::from( "hello" );
+
+        s.push_str(", workd !"); // push_str() appends a literal to a String
+
+        println!("{}", s);
+
+
+
+}
+
+
